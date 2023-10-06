1127cf71b05981d76b8a0861c926354def9de3fa
yese888 <118493093+yese888@users.noreply.github.com>
2023-10-06 10:27:00 +0800

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -16,7 +16,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -E -i 's/account = .*/account = 0xf19Ef76c4f7243E18a12FfCD71bA884673A5EAdB/' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
